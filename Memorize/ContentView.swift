553fb1e37510199d09e854d41898d29316d912c3
moringchen88 <christ-cai@163.com>
2020-10-21 13:55:30 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by caichen on 2020/10/21.
+//  Copyright © 2020 caichen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
