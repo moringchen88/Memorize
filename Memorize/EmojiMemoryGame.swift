b548b3e077aa1ade3e5816f4a93337ba85d00b47
moringchen88 <christ-cai@163.com>
2020-10-28 16:08:47 +0800

    create P3

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,8 +8,8 @@
 
 import SwiftUI
 
-class EmojiMemoryGame{
-    private var model : MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+class EmojiMemoryGame : ObservableObject {
+    @Published private var model : MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis : Array<String> = ["😱","🎃","🕷"]
@@ -22,7 +22,7 @@ class EmojiMemoryGame{
     //MARK: - Access to the model
     var cards: Array<MemoryGame<String>.Card>
     {
-        return model.cards
+        model.cards
     }
     
     //MARK: - Intent(s)
