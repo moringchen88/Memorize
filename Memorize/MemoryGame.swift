e900bba0727bf4b93d1a25ff232437fcb4debfe0
moringchen88 <christ-cai@163.com>
2020-10-30 16:18:52 +0800

    create p4-1

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -12,19 +12,9 @@ struct MemoryGame<CardContent>{
 
     mutating func choose(card:Card)  {
         print("card chosen:\(card)")
-        let chosenIndex : Int = self.index(of: card)
+        let chosenIndex : Int = cards.firstIndex(matching: card)
         self.cards[chosenIndex].isFaceUp = !self.cards[chosenIndex].isFaceUp
     }
-    
-    func index(of card: Card)->Int{
-        for index in 0..<self.cards.count{
-            if self.cards[index].id == card.id{
-                return index
-            }
-        }
-        return 0 //TODO: bogus!
-    }
-    
     init(numberOfParisOfCards:Int,cardContentFactory: (Int) ->CardContent) {
         cards = Array<Card>()
         for pairIndex in 0..<numberOfParisOfCards{
