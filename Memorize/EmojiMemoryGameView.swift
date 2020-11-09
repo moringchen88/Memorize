e1fb7e2d420a7fad49e3675da810dbaa5d21f4e5
moringchen88 <christ-cai@163.com>
2020-11-09 15:53:11 +0800

    create p5

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -25,7 +25,9 @@ struct EmojiMemoryGameView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
 
@@ -37,28 +39,22 @@ struct CardView: View {
             self.body(for:geometry.size)
         }
     }
-    
-    func body(for size :CGSize) ->some View{
-        ZStack{
-            if self.card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth:edgeLineWidth)
+    @ViewBuilder
+    private func body(for size :CGSize) ->some View{
+        if card.isFaceUp || !card.isMatched{
+            ZStack{
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90),clockwise: true).padding(5).opacity(0.4)
                 Text(card.content)
+                    .font(Font.system(size: fontSize(for: size)))
             }
-            else {
-                if !card.isMatched{
-                    RoundedRectangle(cornerRadius:cornerRadius).fill()
-                }
-            }
+            .cardify(isFaceUp: card.isFaceUp)
         }
-        .font(Font.system(size: fontSize(for: size)))
         
     }
     
     //MARK : - Drawing Constants
-    let cornerRadius :CGFloat = 10.0
-    let edgeLineWidth : CGFloat = 3
-    func fontSize(for size:CGSize) ->CGFloat{
-        min(size.width,size.height) * 0.75
+    
+    private func fontSize(for size:CGSize) ->CGFloat{
+        min(size.width,size.height) * 0.7
     }
 }
