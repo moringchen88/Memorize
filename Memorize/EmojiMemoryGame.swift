c7b1463069f066019c25354b21371c30c387a5bc
moringchen88 <christ-cai@163.com>
2020-11-11 16:31:00 +0800

    create p6

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -31,4 +31,9 @@ class EmojiMemoryGame : ObservableObject {
     {
         model.choose(card: card)
     }
+    
+    func resetGame(){
+        model = EmojiMemoryGame.createMemoryGame()
+    }
+    
 }
